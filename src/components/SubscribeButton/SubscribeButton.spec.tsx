708c993a86217de2806fec501dc876f27bdf2bfd
atirson <fabiano.oliveira2213@gmail.com>
2022-02-23 19:55:36 -0300

    bug: Test of subscritionButton dont work with mocked useRouter

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import { useSession } from 'next-auth/client'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { useSession, signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { SubscribeButton } from '.'
 
 jest.mock('next-auth/client')
@@ -15,5 +16,36 @@ describe('SignInButton component', () => {
     expect(screen.getByText('Subscribe now')).toBeInTheDocument()
   })
 
+  it('redirects user to sign in when not authenticated', () => {
+    const signInMocked = signIn as jest.Mock
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText('Subscribe now')
+
+    fireEvent.click(subscribeButton)
+
+    expect(signInMocked).toHaveBeenCalled()
+
+  })
+
+  it('redirects to posts when user already has a subscription', () => {
+    const useRouterMocked =  useRouter as jest.Mock
+    const useSessionMocked = useSession as jest.Mock
+
+    useSessionMocked.mockReturnValueOnce([{user: {name: 'John Doe', email: 'john.doe@example.com'}, activeSubscription: 'fake-active' ,expires: 'fake-expires'}, false])
+
+    const pushMock = jest.fn()
+
+    useRouterMocked.mockReturnValueOnce({ push: pushMock  })
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText('Subscribe now')
+
+    fireEvent.click(subscribeButton)
+
+    expect(useRouterMocked).toHaveBeenCalled()
+  })
 })
 
